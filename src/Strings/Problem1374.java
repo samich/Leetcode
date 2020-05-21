7fa6f329cfc010c367c6b5206bb69f4e4724b387
Sami <itssami@gmail.com>
2020-05-21 11:54:04 +0300

    initial commit

--- src/Strings/Problem1374.java
+++ src/Strings/Problem1374.java
@@ -1,5 +1,16 @@
 package Strings;
 
+/**
+ * 1374. Generate a String With Characters That Have Odd Counts
+ *
+ * Given an integer n, return a string with n characters such that each character in such
+ * string occurs an odd number of times.
+ *
+ * The returned string must contain only lowercase English letters. If there are multiples
+ * valid strings, return any of them.
+ *
+ * */
+
 public class Problem1374 {
 
     public static void main(String[] args) {
