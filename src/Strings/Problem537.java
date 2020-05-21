4abffbce074a47d538ccfd67990d960cd60af829
Sami <itssami@gmail.com>
2020-05-21 22:33:16 +0300

    initial commit

--- src/Strings/Problem537.java
+++ src/Strings/Problem537.java
@@ -1,5 +1,27 @@
 package Strings;
 
+/**
+ * 537. Complex Number Multiplication
+ *
+ *  Given two strings representing two complex numbers.
+ *
+ * You need to return a string representing their multiplication.
+ * Note i2 = -1 according to the definition.
+ *
+ * Example 1:
+ *
+ * Input: "1+1i", "1+1i"
+ * Output: "0+2i"
+ * Explanation: (1 + i) * (1 + i) = 1 + i2 + 2 * i = 2i, and you need convert it to the form of 0+2i.
+ *
+ * Example 2:
+ *
+ * Input: "1+-1i", "1+-1i"
+ * Output: "0+-2i"
+ * Explanation: (1 - i) * (1 - i) = 1 + i2 - 2 * i = -2i, and you need convert it to the form of 0+-2i.
+ *
+ * */
+
 public class Problem537 {
 
     public static void main(String[] args) {
