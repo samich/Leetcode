6a4cb1029d33066bb74027bc436658ac21b3c9b8
Sami <itssami@gmail.com>
2020-05-22 12:59:49 +0300

    initial commit

--- src/Strings/Problem942.java
+++ src/Strings/Problem942.java
@@ -1,8 +1,47 @@
 package Strings;
 
+import java.util.Arrays;
+
+/**
+ * 942. DI String Match
+ *
+ * Given a string S that only contains "I" (increase) or "D" (decrease), let N = S.length.
+ * Return any permutation A of [0, 1, ..., N] such that for all i = 0, ..., N-1:
+ *
+ * If S[i] == "I", then A[i] < A[i+1]
+ * If S[i] == "D", then A[i] > A[i+1]
+ *
+ * Example 1:
+ *
+ * Input: "IDID"
+ * Output: [0,4,1,3,2]
+ *
+ * Example 2:
+ *
+ * Input: "III"
+ * Output: [0,1,2,3]
+ *
+ * Example 3:
+ *
+ * Input: "DDI"
+ * Output: [3,2,0,1]
+ *
+ * */
+
 public class Problem942 {
     public static void main(String[] args) {
 
+        //example 1:
+        String s = "IDID";
+        System.out.println(Arrays.toString(diStringMatch(s)));
+
+        //example 2:
+        s = "III";
+        System.out.println(Arrays.toString(diStringMatch(s)));
+
+        //example 3:
+        s = "DDI";
+        System.out.println(Arrays.toString(diStringMatch(s)));
 
     }
 
