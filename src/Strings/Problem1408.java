ad77d62df0cd1e68ff206647bfa2c0cd0f4fd142
Sami <itssami@gmail.com>
2020-05-25 18:38:11 +0300

    initial commit

--- src/Strings/Problem1408.java
+++ src/Strings/Problem1408.java
@@ -3,9 +3,31 @@ package Strings;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 1408. String Matching in an Array
+ *
+ * Given an array of string words. Return all strings in words which is substring of another word in any order.
+ *
+ * String words[i] is substring of words[j], if can be obtained removing some characters to left and/or right
+ * side of words[j].
+ *
+ * */
+
 public class Problem1408 {
 
     public static void main(String[] args) {
+
+        //example 1:
+        String[] s = {"mass", "as", "hero", "superhero"};
+        //System.out.println(stringMatching(s).toString());
+
+        //example 2:
+        String[] s1 = {"blue","green","bu"};
+        //System.out.println(stringMatching(s1).toString());
+
+        //example 3:
+        String[] s2 = { "uexk","aeuexkf","wgxih","yuexk","gxea","yuexkm","ypmfx","jjuexkmb","wqpri","aeuexkfpo","kqtnz","pkqtnza","nrbb","hmypmfx","krqs","jjuexkmbyt","zvru","ypmfxj"};
+        System.out.println(stringMatching(s2).toString());
 
     }
 
@@ -15,6 +37,7 @@ public class Problem1408 {
 
         String appendStr = "";
 
+        //concat all words together
         for(String word : words) {
             appendStr = appendStr.concat(word).concat("|");
         }
