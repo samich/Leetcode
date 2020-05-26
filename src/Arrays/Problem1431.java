cc7a1484ce244e732c94aa453d4c7ac6a832a70b
Sami <itssami@gmail.com>
2020-05-26 19:38:38 +0300

    initial commit

---
+++ src/Arrays/Problem1431.java
@@ -0,0 +1,38 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem1431 {
+
+    public static void main(String[] args) {
+        int[] candies = {12,1,12};
+        int extraCandies = 10;
+
+        System.out.println(kidsWithCandies(candies, extraCandies).toString());
+    }
+
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+        List<Boolean> list = new ArrayList<>();
+
+        int max = candies[0];
+
+        for ( int i = 1; i < candies.length; i++ ) {
+
+            if ( candies[i] > max ) {
+                max = candies[i];
+            }
+        }
+
+        for (int candy : candies) {
+
+
+            list.add(candy + extraCandies >= max);
+        }
+
+        return list;
+
+    }
+
+}
