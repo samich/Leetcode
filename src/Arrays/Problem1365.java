fbf3cc0f0f7b303d530ab3b687fb988f946171b1
Sami <itssami@gmail.com>
2020-05-27 14:53:45 +0300

    initial commit

---
+++ src/Arrays/Problem1365.java
@@ -0,0 +1,39 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class Problem1365 {
+
+    public static void main(String[] args) {
+
+        int[] nums = { 8, 1, 2, 2, 3 };
+
+        System.out.println(Arrays.toString(smallerNumbersThanCurrent(nums)));
+
+    }
+
+    public static int[] smallerNumbersThanCurrent(int[] nums) {
+
+        int[] arr = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int count = 0;
+
+            for (int j = 0; j < nums.length; j++) {
+
+                if ( nums[i] > nums[j] ) {
+                    count++;
+                }
+
+            }
+
+            arr[i] = count;
+
+        }
+
+        return arr;
+
+    }
+
+}
