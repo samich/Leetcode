6034453d2f294813aea86265ce996d93621fd86b
Sami <itssami@gmail.com>
2020-05-27 14:57:20 +0300

    initial commit

--- src/Arrays/Problem1365.java
+++ src/Arrays/Problem1365.java
@@ -2,6 +2,16 @@ package Arrays;
 
 import java.util.Arrays;
 
+/**
+ * 1365. How Many Numbers Are Smaller Than the Current Number
+ *
+ * Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
+ * That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+ *
+ * Return the answer in an array.
+ *
+ * */
+
 public class Problem1365 {
 
     public static void main(String[] args) {
