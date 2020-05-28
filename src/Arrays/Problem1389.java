2329e83e84bde16d6d6a790211329f8e73c72dbb
Sami <itssami@gmail.com>
2020-05-28 12:39:39 +0300

    initial commit

--- src/Arrays/Problem1389.java
+++ src/Arrays/Problem1389.java
@@ -4,6 +4,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * 1389. Create Target Array in the Given Order
+ *
+ * Given two arrays of integers nums and index. Your task is to create target array under
+ * the following rules:
+ *
+ * Initially target array is empty.
+ * From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+ * Repeat the previous step until there are no elements to read in nums and index.
+ *
+ * Return the target array.
+ *
+ * It is guaranteed that the insertion operations will be valid.
+ *
+ * */
+
 public class Problem1389 {
 
     public static void main(String[] args) {
