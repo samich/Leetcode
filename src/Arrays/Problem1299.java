538798ac40617022304d5fa4ea487799f5023a44
Sami <itssami@gmail.com>
2020-05-29 11:02:12 +0300

    initial commit

--- src/Arrays/Problem1299.java
+++ src/Arrays/Problem1299.java
@@ -2,6 +2,16 @@ package Arrays;
 
 import java.util.Arrays;
 
+/**
+ * 1299. Replace Elements with Greatest Element on Right Side
+ *
+ * Given an array arr, replace every element in that array with the greatest element among the elements
+ * to its right, and replace the last element with -1.
+ *
+ * After doing so, return the array.
+ *
+ * */
+
 public class Problem1299 {
     public static void main(String[] args) {
 
