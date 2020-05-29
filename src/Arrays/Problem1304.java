863d467e573e2ade10d5aedb3574c6a10c11b310
Sami <itssami@gmail.com>
2020-05-29 12:20:53 +0300

    initial commit

---
+++ src/Arrays/Problem1304.java
@@ -0,0 +1,52 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Problem1304 {
+
+    public static void main(String[] args) {
+
+        //example 1:
+        int n = 5;
+        System.out.println(Arrays.toString(sumZero(n)));
+
+        //example 2:
+        n = 7;
+        System.out.println(Arrays.toString(sumZero(n)));
+
+        //example 3:
+        n = 23;
+        System.out.println(Arrays.toString(sumZero(n)));
+
+    }
+
+    public static int[] sumZero(int n) {
+
+        List<Integer> list = new ArrayList<>();
+
+        for ( int i = 0; i < n -1; i += 2 ) {
+            int a;
+            a = (int) (Math.random() * 1000) + 1;
+
+            while ( list.contains(a) ) {
+                a = (int) (Math.random() * 1000) + 1;
+            }
+
+            list.add(a);
+            list.add(-a);
+        }
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < list.size(); i++) {
+            arr[i] = list.get(i);
+        }
+
+        return arr;
+
+    }
+
+
+}
