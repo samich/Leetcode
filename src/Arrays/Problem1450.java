efd00faf4d9528ae473ef89aeb11d39dd8c76e32
Sami <itssami@gmail.com>
2020-05-29 10:25:30 +0300

    initial commit

--- src/Arrays/Problem1450.java
+++ src/Arrays/Problem1450.java
@@ -1,5 +1,16 @@
 package Arrays;
 
+/**
+ * 1450. Number of Students Doing Homework at a Given Time
+ * Given two integer arrays startTime and endTime and given an integer queryTime.
+ *
+ * The ith student started doing their homework at the time startTime[i] and finished it at time endTime[i].
+ *
+ * Return the number of students doing their homework at time queryTime. More formally, return the number of
+ * students where queryTime lays in the interval [startTime[i], endTime[i]] inclusive.
+ *
+*/
+
 public class Problem1450 {
     public static void main(String[] args) {
 
