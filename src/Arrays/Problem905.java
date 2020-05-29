800094c9683710db6c3f41397390ae9a2984ad39
Sami <itssami@gmail.com>
2020-05-29 15:36:17 +0300

    initial commit

--- src/Arrays/Problem905.java
+++ src/Arrays/Problem905.java
@@ -2,6 +2,16 @@ package Arrays;
 
 import java.util.Arrays;
 
+/**
+ * 905. Sort Array By Parity
+ *
+ * Given an array A of non-negative integers, return an array consisting of all the
+ * even elements of A, followed by all the odd elements of A.
+ *
+ * You may return any answer array that satisfies this condition.
+ *
+ * */
+
 public class Problem905 {
 
     public static void main(String[] args) {
