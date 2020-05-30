95637c680bbf1b4785ffc5d41dd117a903b0d29e
Sami <itssami@gmail.com>
2020-05-30 20:16:43 +0300

    initial commit

--- src/Arrays/Problem238_1.java
+++ src/Arrays/Problem238_1.java
@@ -2,6 +2,21 @@ package Arrays;
 
 import java.util.Arrays;
 
+/**
+ * 238. Product of Array Except Self
+ *
+ * Given an array nums of n integers where n > 1,  return an array output such that output[i]
+ * is equal to the product of all the elements of nums except nums[i].
+ *
+ * Note: This is according to the following requirements:
+ * Note: Please solve it without division and in O(n).
+ *
+ * The idea is that we calculate the product of integers on the left, and on the right and then multiply
+ * when calculating the left, the first index has to be one because for example if array has "3", it is
+ * possible only after it multiplied by 1.
+ * same with the right most index when calculating product of integers on the right of the integer
+ * */
+
 public class Problem238_1 {
 
     public static void main(String[] args) {
