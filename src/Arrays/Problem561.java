8fb6dfb7441ebb97ec93d5d429ae35bd72dafff0
Sami <itssami@gmail.com>
2020-05-30 11:42:59 +0300

    initial commit

--- src/Arrays/Problem561.java
+++ src/Arrays/Problem561.java
@@ -2,11 +2,20 @@ package Arrays;
 
 import java.util.Arrays;
 
+/**
+ * 561. Array Partition I
+ *
+ *  Given an array of 2n integers, your task is to group these integers into n pairs of integer,
+ *  say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n
+ *  as large as possible.
+ *
+ * */
+
 public class Problem561 {
     public static void main(String[] args) {
 
+        //example 1:
         int[] nums = {1, 4, 3, 2};
-
         System.out.println(arrayPairSum(nums));
 
     }
