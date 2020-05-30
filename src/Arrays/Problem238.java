9bcd6d1739196cb027bb81ec2b5f68244e4bb0a7
Sami <itssami@gmail.com>
2020-05-30 15:22:32 +0300

    initial commit

--- src/Arrays/Problem238.java
+++ src/Arrays/Problem238.java
@@ -2,12 +2,20 @@ package Arrays;
 
 import java.util.Arrays;
 
-
+/**
+ * 238. Product of Array Except Self
+ *
+ * Given an array nums of n integers where n > 1,  return an array output such that output[i] is
+ * equal to the product of all the elements of nums except nums[i].
+ *
+ * (Note: This is very slow approach, it should be done in O(n).
+ * */
 
 public class Problem238 {
 
     public static void main(String[] args) {
 
+        //example 1:
         int[] nums = {1, 2, 3, 4};
         System.out.println(Arrays.toString(productExceptSelf(nums)));
 
@@ -27,7 +35,6 @@ public class Problem238 {
                 if ( i != j ) {
                     val *= nums[j];
                 }
-
 
             }
 
