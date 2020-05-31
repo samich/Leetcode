af6aeebd153d25a6bf89d77a7d21346fdb26dc20
Sami <itssami@gmail.com>
2020-05-31 18:30:38 +0300

    initial commit

--- src/Arrays/Problem1460.java
+++ src/Arrays/Problem1460.java
@@ -9,8 +9,17 @@ public class Problem1460 {
         //example 1:
         int[] target  = {1, 2, 3, 4, 5};
         int[] arr  = {2, 4, 1, 3, 7};
-
         System.out.println( canBeEqual(target, arr) );
+
+        //example 2:
+        int[] target1  = {7};
+        int[] arr1  = {7};
+        System.out.println( canBeEqual(target1, arr1) );
+
+        //example 3:
+        int[] target2  = {1, 12};
+        int[] arr2  = {12, 1};
+        System.out.println( canBeEqual(target2, arr2) );
 
     }
 
