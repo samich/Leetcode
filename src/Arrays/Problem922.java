1a8b0630b3aa7ca8c49259b5b7ad6cfdd7f88ab7
Sami <itssami@gmail.com>
2020-06-01 17:46:37 +0300

    initial commit

--- src/Arrays/Problem922.java
+++ src/Arrays/Problem922.java
@@ -2,6 +2,17 @@ package Arrays;
 
 import java.util.Arrays;
 
+/**
+ * 922. Sort Array By Parity II
+ *
+ * Given an array A of non-negative integers, half of the integers in A are odd,
+ * and half of the integers are even.
+ *
+ * Sort the array so that whenever A[i] is odd, i is odd; and whenever A[i] is even, i is even.
+ * You may return any answer array that satisfies this condition.
+ *
+ * */
+
 public class Problem922 {
 
     public static void main(String[] args) {
@@ -19,16 +30,19 @@ public class Problem922 {
 
         while (even < A.length && odd < A.length) {
 
+            //if there is not even integer at even index
             if ( A[even] % 2 != 0) {
 
                 int temp = A[even];
                 A[even] = A[odd];
                 A[odd] = temp;
 
+                //as we have already moved an odd integer, so increase its index
                 odd += 2;
 
             } else {
 
+                //if
                 even += 2;
 
             }
