acbac876f868648ed5ea73976ebbc7db11daad30
Sami <itssami@gmail.com>
2020-06-01 14:17:11 +0300

    initial commit

--- src/Arrays/Problem1385.java
+++ src/Arrays/Problem1385.java
@@ -1,5 +1,17 @@
 package Arrays;
 
+/**
+ * 1385. Find the Distance Value Between Two Arrays
+ *
+ * Given two integer arrays arr1 and arr2, and the integer d, return the
+ * distance value between the two arrays.
+ *
+ * The distance value is defined as the number of elements arr1[i] such that
+ * there is not any element arr2[j] where |arr1[i]-arr2[j]| <= d.
+ *
+ *
+*/
+
 public class Problem1385 {
 
     public static void main(String[] args) {
