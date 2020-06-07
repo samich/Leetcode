3ad1e023cc44fa1a67c14daa01cf2bde1c0f7780
Sami <itssami@gmail.com>
2020-06-07 17:57:22 +0300

    initial commit

--- src/Stack/Problem85.java
+++ src/Stack/Problem85.java
@@ -1,7 +1,14 @@
 package Stack;
 
-import java.util.Arrays;
 import java.util.Stack;
+
+/**
+ * 85. Maximal Rectangle
+ *
+ * Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's
+ * and return its area.
+ *
+ * */
 
 public class Problem85 {
 
