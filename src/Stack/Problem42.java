e5081494c723b9cfd07e0108f662cdcb5bbef4ea
Sami <itssami@gmail.com>
2020-06-08 14:27:09 +0300

    initial commit

--- src/Stack/Problem42.java
+++ src/Stack/Problem42.java
@@ -2,6 +2,14 @@ package Stack;
 
 import java.util.Stack;
 
+/**
+ * 42. Trapping Rain Water
+ *
+ * Given n non-negative integers representing an elevation map where the width of each bar is 1,
+ * compute how much water it is able to trap after raining.
+ *
+ * */
+
 public class Problem42 {
 
     public static void main(String[] args) {
