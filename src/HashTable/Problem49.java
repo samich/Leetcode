d71caddecb4b6b948dcfdb7332e79c96fc2f23f2
Sami <itssami@gmail.com>
2020-06-10 20:56:52 +0300

    initial commit

--- src/HashTable/Problem49.java
+++ src/HashTable/Problem49.java
@@ -1,10 +1,39 @@
 package HashTable;
 
+import java.util.*;
+
 public class Problem49 {
 
     public static void main(String[] args) {
 
+
+
     }
+
+    private static List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for ( String s: strs ) {
+
+            char[] chArr = s.toCharArray();
+
+            Arrays.sort(chArr);
+
+            String sorted = new String(chArr);
+
+            if ( !map.containsKey(sorted) ) {
+                map.put(sorted, new LinkedList<>());
+            }
+
+            map.get(sorted).add(s);
+        }
+
+        return new LinkedList<>( map.values() );
+
+    }
+
+
 
 
 }
